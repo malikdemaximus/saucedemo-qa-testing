3e3a451a5f4a98201bc4447b0ac93b0bd357fb16
Abdulmalik Yermekbay <malikdemaximus@users.noreply.github.com>
2025-05-05 08:57:01 +0300

    Update conftest.py

--- autotests/conftest.py
+++ autotests/conftest.py
@@ -1,6 +1,16 @@
 import pytest  
 from selenium import webdriver  
 from selenium.webdriver.chrome.options import Options  
+import allure
+
+@pytest.fixture
+def attach_screenshot(browser):
+    yield
+    allure.attach(
+        browser.get_screenshot_as_png(),
+        name="screenshot",
+        attachment_type=allure.attachment_type.PNG
+    )
 
 @pytest.fixture(scope="function")  
 def browser():  
