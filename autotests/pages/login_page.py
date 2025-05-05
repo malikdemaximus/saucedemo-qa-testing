9b09b1b49e1719c710d81db9ba54ee2b54048922
Abdulmalik Yermekbay <malikdemaximus@users.noreply.github.com>
2025-05-05 09:33:53 +0300

    Update login_page.py

--- autotests/pages/login_page.py
+++ autotests/pages/login_page.py
@@ -1,16 +1,11 @@
-from selenium.webdriver.common.by import By
 from .base_page import BasePage
 
 class LoginPage(BasePage):
-    USERNAME_INPUT = (By.ID, "user-name")
-    PASSWORD_INPUT = (By.ID, "password")
+    USERNAME = (By.ID, "user-name")
+    PASSWORD = (By.ID, "password")
     LOGIN_BUTTON = (By.ID, "login-button")
-    ERROR_MESSAGE = (By.CSS_SELECTOR, ".error-message-container")
-
+    
     def login(self, username, password):
-        self.find_element(self.USERNAME_INPUT).send_keys(username)
-        self.find_element(self.PASSWORD_INPUT).send_keys(password)
+        self.find_element(self.USERNAME).send_keys(username)
+        self.find_element(self.PASSWORD).send_keys(password)
         self.find_element(self.LOGIN_BUTTON).click()
-
-    def get_error_message(self):
-        return self.find_element(self.ERROR_MESSAGE).text
