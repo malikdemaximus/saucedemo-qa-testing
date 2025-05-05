8a5e949b7e3d474fec86c03bc357e1870914002d
Abdulmalik Yermekbay <malikdemaximus@users.noreply.github.com>
2025-05-05 08:55:19 +0300

    Update test_login.py

--- autotests/test_login.py
+++ autotests/test_login.py
@@ -1,14 +1,13 @@
+import pytest
 from pages.login_page import LoginPage
 
-def test_successful_login(browser):
+@pytest.mark.parametrize("username, password, expected", [
+    ("standard_user", "secret_sauce", True),
+    ("locked_out_user", "secret_sauce", False),
+    ("invalid", "invalid", False)
+])
+def test_login_with_params(browser, username, password, expected):
     login_page = LoginPage(browser)
-    login_page.login("standard_user", "secret_sauce")
-    assert "inventory.html" in browser.current_url
-    
-def test_invalid_login(browser):  
-    browser.get("https://www.saucedemo.com/")  
-    browser.find_element("id", "user-name").send_keys("locked_out_user")  
-    browser.find_element("id", "password").send_keys("wrong_pass")  
-    browser.find_element("id", "login-button").click()  
-    error = browser.find_element("css selector", ".error-message-container").text  
-    assert "Epic sadface" in error, "Error message not found!"  
+    login_page.login(username, password)
+    assert ("inventory.html" in browser.current_url) == expected
+
