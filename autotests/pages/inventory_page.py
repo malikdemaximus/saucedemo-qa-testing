fb2676ad606e42b34f8d4bd27540c5c4103e54eb
Abdulmalik Yermekbay <malikdemaximus@users.noreply.github.com>
2025-05-06 11:25:51 +0500

    Update inventory_page.py

--- autotests/pages/inventory_page.py
+++ autotests/pages/inventory_page.py
@@ -1,8 +1,17 @@
-from .base_page import BasePage
 from selenium.webdriver.common.by import By
+from .base_page import BasePage
 
 class InventoryPage(BasePage):
-    ITEM_NAMES = (By.CLASS_NAME, "inventory_item_name")
-    
-    def get_items_count(self):
-        return len(self.find_elements(self.ITEM_NAMES))
+    TITLE = (By.CLASS_NAME, "title")
+    ITEMS = (By.CLASS_NAME, "inventory_item")
+    ADD_TO_CART_BUTTON = (By.XPATH, "//button[contains(@id,'add-to-cart')]")
+    CART_BADGE = (By.CLASS_NAME, "shopping_cart_badge")
+
+    def is_opened(self):
+        return "inventory" in self.driver.current_url
+
+    def add_first_item_to_cart(self):
+        self.find_elements(self.ADD_TO_CART_BUTTON)[0].click()
+
+    def get_cart_count(self):
+        return self.find_element(self.CART_BADGE).text
